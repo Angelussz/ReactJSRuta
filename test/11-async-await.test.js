77409287112b5588301bb365ec45e56665622a36
angelo <aperezr@unsa.edu.pe>
2023-11-14 11:41:52 -0500

    agregar jestconfig y jestSetup para configuraciones personalizadas y agregar whatwg-fetch para node < 18

---
+++ test/11-async-await.test.js
@@ -0,0 +1,8 @@
+import { getImagen } from "../src/base-pruebas/11-async-await"
+
+describe("Test 11-async-await con jest",()=>{
+    test("Test de la funcion getImage",async()=>{
+        const url = await getImagen();
+        expect(typeof url).toBe("string");
+    })
+})
